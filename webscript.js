f1990772c60fed6410362d55af29bc4715a2ac48
PhilipEsg <philipjamesesguerra@gmail.com>
2026-03-27 13:10:43 -0700

    set user score and comp score to local scope within playGame function, and placed playRound function delcaration in playGame function

--- webscript.js
+++ webscript.js
@@ -32,40 +32,6 @@ function getHumanChoice() {
 
 
 
-//gameplay for a single round
-function playRound(user_choice, comp_choice) {
-    // Case 1: computer wins
-    if(user_choice == "rock" && comp_choice == "paper") {
-        console.log("You lose! Paper beats rock!");
-        comp_score = comp_score + 1;
-    }
-    else if (user_choice == "paper" && comp_choice == "scissors") {
-        console.log("You lose! Scissors cuts paper!");
-        comp_score = comp_score + 1;
-    }
-    else if (user_choice == "scissors" && comp_choice == "rock") {
-        console.log("You lose! Rock beats scissors!");
-        comp_score = comp_score + 1;
-    }
-    // Case 2: User wins
-    if(user_choice == "paper" && comp_choice == "rock") {
-        console.log("You win! Paper beats rock!");
-        user_score = user_score + 1;
-    }
-    else if (user_choice == "scissors" && comp_choice == "paper") {
-        console.log("You win! Scissors cuts paper!");
-        user_score = user_score + 1;
-    }
-    else if (user_choice == "rock" && comp_choice == "scissors") {
-        console.log("You win! Rock beats scissors!");
-        user_score = user_score + 1;
-    }
-    // Case 3: Draw
-    else {
-        console.log("Draw! No winner!");
-    }
-}
-
 
 
 function playGame() {
@@ -73,6 +39,46 @@ function playGame() {
     let comp_choice = "";   // computer's choice
     let user_choice = "";   // user's choice
     let round_num = 0;      // current round number
+    let comp_score = 0;     // initial computer's score during gameplay
+    let user_score = 0;     // initial user's score during gameplay
+
+
+
+    //gameplay for a single round
+    function playRound(user_choice, comp_choice) {
+        // Case 1: computer wins
+        if(user_choice == "rock" && comp_choice == "paper") {
+            console.log("You lose! Paper beats rock!");
+            comp_score = comp_score + 1;
+        }
+        else if (user_choice == "paper" && comp_choice == "scissors") {
+            console.log("You lose! Scissors cuts paper!");
+            comp_score = comp_score + 1;
+        }
+        else if (user_choice == "scissors" && comp_choice == "rock") {
+            console.log("You lose! Rock beats scissors!");
+            comp_score = comp_score + 1;
+        }
+        // Case 2: User wins
+        if(user_choice == "paper" && comp_choice == "rock") {
+            console.log("You win! Paper beats rock!");
+            user_score = user_score + 1;
+        }
+        else if (user_choice == "scissors" && comp_choice == "paper") {
+            console.log("You win! Scissors cuts paper!");
+            user_score = user_score + 1;
+        }
+        else if (user_choice == "rock" && comp_choice == "scissors") {
+            console.log("You win! Rock beats scissors!");
+            user_score = user_score + 1;
+        }
+        // Case 3: Draw
+        else {
+            console.log("Draw! No winner!");
+        }
+    }
+
+
 
     while(round_num < 5) {
         comp_choice = getComputerChoice();
@@ -96,8 +102,6 @@ function playGame() {
 }
 
 
-let comp_score = 0;     // initial computer's score during gameplay
-let user_score = 0;     // initial user's score during gameplay
 playGame();
 
 
