8752613758588cd5c8311271090ebf5dbbde7bc7
PhilipEsg <philipjamesesguerra@gmail.com>
2026-03-27 11:23:45 -0700

    Added function to get random choice from computer

---
+++ webscript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    var num = 0;
+    num = Math.random();
+
+    return num; 
+}
+
+let random_num = 0;
+random_num = getComputerChoice;
+
+console.log(random_num);
